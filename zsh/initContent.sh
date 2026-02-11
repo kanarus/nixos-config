ce478a8210a66619800b48e8010054984753aab8
kanarus <mail@kanarus.dev>
2026-02-12 03:30:25 +0900

    zsh: git status color

--- zsh/initContent.sh
+++ zsh/initContent.sh
@@ -6,7 +6,7 @@ bindkey ";5D" backward-word
 function git_status_color() {
   git_status_output=$(git status --short)
   if [ -z $git_status_output ]; then
-    echo '121'
+    echo '122'
   else
     echo '124'
   fi
