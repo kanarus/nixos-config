e3e348e081e54245e2be98b730caf9fc18e1aa82
kanarus <mail@kanarus.dev>
2026-02-14 06:40:57 +0900

    homemanager: i18n.input-method: `waylandFrontend = true;`

--- home.nix
+++ home.nix
@@ -25,86 +25,6 @@
     "${config.xdg.dataHome}/wallpaper/default.png".source = ./assets/nix-wallpaper-gear.png;
   };
 
-  # systemd.user.services = {
-  #   swaybg = {
-  #     Unit = {
-  #       Description = "Wallpaper solution for Wayland";
-  #       Documentation = "https://github.com/swaywm/swaybg";
-  #       PartOf = "graphical-session.target";
-  #       After = "graphical-session.target";
-  #       Requisite = "graphical-session.target";
-  #     };
-  #     Service = {
-  #       ExecStart = "${pkgs.swaybg}/bin/swaybg -i ${wallpaper} -m fill -o *";
-  #       Restart = "on-failure";
-  #     };
-  #     Install = {
-  #       WantedBy = [ "graphical-session.target" ];
-  #     };
-  #   };
-  # };
-
-  # wayland.windowManager.hyprland = {
-  #   enable = true;
-
-  #   xwayland.enable = true;
-
-  #   settings = {
-  #     monitor = ",2560x1440@60,0x0,1";
-
-  #     "$mod" = "SUPER";
-  #     "$terminal" = "ghostty";
-  #     "$browser" = "google-chrome-stable"; # pkg `google-chrome` provides command `google-chrome-stable`!
-  #     "$menu" = "wofi --show drun";
-
-  #     exec-once = [
-  #       "sleep 5 && spice-vdagent -s /run/spice-vdagentd/spice-vdagent-sock"
-  #       "wl-paste --watch cliphist store"
-  #     ];
-
-  #     general = {
-  #       gaps_in = 5;
-  #       gaps_out = 10;
-  #       border_size = 2;
-  #       "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-  #       "col.inactive_border" = "rgba(595959aa)";
-  #       layout = "dwingle";
-  #     };
-      
-  #     decoration = {
-  #       rounding = 10;
-  #       blur = {
-  #         enabled = true;
-  #         size = 3;
-  #         passes = 1;
-  #       };
-  #     };
-      
-  #     bind = [
-  #       "$mod, T, exec, $terminal"
-  #       "$mod, B, exec, $browser"
-  #       # "$mod, E, exec, dolphin"   # file manager
-  #       "$mod, Space, exec, $menu" # wofi is recommended
-
-  #       "$mod, C, killactive,"
-  #       "$mod, M, exit,"
-  #       "$mod, V, togglefloating,"
-  #       "$mod, P, pseudo,"
-  #       "$mod, J, togglesplit,"
-
-  #       "$mod, left, movefocus, l"
-  #       "$mod, right, movefocus, r"
-  #       "$mod, up, movefocus, u"
-  #       "$mod, down, movefocus, d"
-  #     ];
-
-  #     bindm = [
-  #       "$mod, mouse:272, movewindow"
-  #       "$mod, mouse:273, resizewindow"
-  #     ];
-  #   };
-  # };  
-
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
@@ -140,6 +60,7 @@
     enable = true;
     type = "fcitx5";
     fcitx5 = {
+      waylandFrontend = true;
       addons = [ pkgs.fcitx5-mozc ];
       settings.inputMethod = {
         GroupOrder."0" = "Default";
