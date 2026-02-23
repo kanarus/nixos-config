3e80d02ca8de0339683bd54009b0d8ed223ff053
kanarus <mail@kanarus.dev>
2026-02-24 03:16:52 +0900

    :

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,7 +16,7 @@
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           inherit system;
-          
+
           specialArgs = { inherit inputs; };
 
           modules = [
@@ -34,4 +34,3 @@
       };
     };
 }
-
