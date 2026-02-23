4c0f13b05566a8dbec02e4ed4db05a14e88f38ed
kanarus <mail@kanarus.dev>
2026-02-24 06:38:09 +0900

    sddm

--- system/sddm.nix
+++ system/sddm.nix
@@ -14,18 +14,18 @@ let
     installPhase = ''
       cp ${../assets/nix-wallpaper-gear.png} where_is_my_sddm_theme/background.png
       substituteInPlace where_is_my_sddm_theme/theme.conf \
-        --replace 'background='               'background=background.png' \
-        --replace 'backgroundFill=#000000'    'backgroundFill=' \
-        --replace 'font=monospace'             'font="UDEV Gothic 35NF"' \
-        --replace 'helpFont=monospace'         'helpFont="UDEV Gothic 35NF"' \
-        --replace 'passwordCharacter=*'        'passwordCharacter=⬤' \
-        --replace 'passwordFontSize=96'        'passwordFontSize=32' \
-        --replace 'passwordInputWidth=0.5'     'passwordInputWidth=0.75' \
-        --replace 'passwordInputRadius='       'passwordInputRadius=12' \
-        --replace 'passwordInputBorderWidth=0' 'passwordInputBorderWidth=4' \
-        --replace 'passwordInputBorderColor='  'passwordInputBorderColor=#878787' \
-        --replace 'passwordTextColor='         'passwordTextColor=#878787' \
-        --replace 'passwordCursorColor=random' 'passwordCursorColor=#878787'
+        --replace 'background='                     'background=background.png' \
+        --replace 'backgroundFill=#000000'          'backgroundFill=' \
+        --replace 'font=monospace'                  'font="UDEV Gothic 35NF"' \
+        --replace 'helpFont=monospace'              'helpFont="UDEV Gothic 35NF"' \
+        --replace 'passwordInputCursorVisible=true' 'passwordInputCursorVisible=false' \
+        --replace 'passwordInputWidth=0.5'          'passwordInputWidth=0.75' \
+        --replace 'passwordInputRadius='            'passwordInputRadius=12' \
+        --replace 'passwordInputBorderWidth=0'      'passwordInputBorderWidth=4' \
+        --replace 'passwordInputBorderColor='       'passwordInputBorderColor=#5f5f5f' \
+        --replace 'passwordTextColor='              'passwordTextColor=#5f5f5f' \
+        --replace 'passwordCharacter=*'             'passwordCharacter=⬤' \
+        --replace 'passwordFontSize=96'             'passwordFontSize=32'
       mkdir -p $out/share/sddm/themes
       cp -a where_is_my_sddm_theme $out/share/sddm/themes/
     '';
