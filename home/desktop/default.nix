d58bf50048a98fd1ec7dda7362c41d6794ebccca
kanarus <mail@kanarus.dev>
2026-02-26 22:28:14 +0900

    desktop

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -17,10 +17,10 @@ in
   ];
 
   home.file = {
-    "${config.xdg.configHome}/niri/config.kdl".source = ./niri/config.kdl;
-    "${config.xdg.configHome}/waybar/style.css".source = ./waybar/style.css;
-    "${config.xdg.configHome}/waybar/config.jsonc".source = ./waybar/config.jsonc;
-    "${config.xdg.configHome}/waybar/power_menu.xml".source = ./waybar/power_menu.xml;
+    "${config.xdg.configHome}/niri/config.kdl".source = ./niri-config.kdl;
+    "${config.xdg.configHome}/waybar/style.css".source = ./waybar-style.css;
+    "${config.xdg.configHome}/waybar/config.jsonc".source = ./waybar-config.jsonc;
+    "${config.xdg.configHome}/waybar/power_menu.xml".source = ./waybar-powermenu.xml;
     "${config.xdg.dataHome}/wallpaper/default.png".source = ../../assets/nix-wallpaper-gear.png;
   };
 
