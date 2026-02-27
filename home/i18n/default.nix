9aaa856463313f972e86e4cf1891feadcfce075f
kanarus <mail@kanarus.dev>
2026-02-28 03:31:04 +0900

    i18n: fix to use `i18n.inputMethod.fcitx5.settings.addons` instead of direct putting `classicui.conf` in `~/.config/fcitx/conf` that is symlink to `/nix/store/...`

--- home/i18n/default.nix
+++ home/i18n/default.nix
@@ -8,29 +8,33 @@
         fcitx5-mozc
         fcitx5-nord
       ];
-      settings.inputMethod = {
-        GroupOrder."0" = "Default";
-        "Groups/0" = {
-          Name = "Default";
-          "Default Layout" = "us";
-          DefaultIM = "mozc";
+      settings = {
+        inputMethod = {
+          "GroupOrder" = {
+            "0" = "Default";
+          };
+          "Groups/0" = {
+            "Name" = "Default";
+            "Default Layout" = "us";
+            "DefaultIM" = "mozc";
+          };
+          "Groups/0/Items/0" = {
+            "Name" = "keyboard-us";
+            "Layout" = "";
+          };
+          "Groups/0/Items/1" = {
+            "Name" = "mozc";
+            "Layout" = "";
+          };
         };
-        "Groups/0/Items/0" = {
-          Name = "keyboard-us";
-          Layout = "";
-        };
-        "Groups/0/Items/1" = {
-          Name = "mozc";
-          Layout = "";
+        addons = {
+          classicui = {
+            globalSection = {
+              "Theme" = "Nord-Dark";
+            };
+          };
         };
       };
     };
-  };
-
-  home.file = {
-    "${config.xdg.configHome}/fcitx5/conf/classicui.conf".text = ''
-      [ClassicUI]
-      Theme=Nord-Dark
-    '';
   };
 }
