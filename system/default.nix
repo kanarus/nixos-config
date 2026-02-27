46d7a48bfb2e9d725970254a1a26da78f7c9137b
kanarus <mail@kanarus.dev>
2026-02-28 02:44:50 +0900

    system: add programs.ssh with knownHosts with github.com's publicKey

--- system/default.nix
+++ system/default.nix
@@ -50,6 +50,14 @@
     wget
   ];
 
+  programs.ssh = {
+    knownHosts = {
+      "github.com" = {
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      };
+    };
+  };
+
   fonts = {
     fontconfig.enable = true;
     packages = with pkgs; [
