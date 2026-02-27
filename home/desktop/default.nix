76d3b073647b8111ac9b9ba3394c68e4d661e92d
kanarus <mail@kanarus.dev>
2026-02-27 22:20:57 +0900

    desktop: papirus-icon-theme.preInstall: create snake_case symlink for `fcitx-mozc*.svg`s

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,9 +1,26 @@
 { config, pkgs, ... }:
 let
-  cursorTheme = {
+  gtkTheme = {
+    package = pkgs.graphite-gtk-theme;
+    name = "Graphite-Dark";
+  };
+  gtkCursorTheme = {
     package = pkgs.phinger-cursors;
     name = "phinger-cursors-light";
     size = 24;
+  };
+  gtkIconTheme = {
+    # create symlinks named `fcitx_mozc_xxx` for all existing `fcitx-mozc-xxx`
+    package = pkgs.papirus-icon-theme.overrideAttrs (oldAttrs: {
+      preInstall = (oldAttrs or "") + ''
+        for f in $(find Papirus* -type f -name "fcitx-mozc*.svg"); do
+          dir="$(dirname $f)"
+          base="$(basename $f)"
+          ln -sf "$base" "$dir/$(echo $base | tr '-' '_')"
+        done
+      '';
+    });
+    name = "Papirus-Dark";
   };
 in
 {
@@ -24,25 +41,17 @@ in
     "${config.xdg.dataHome}/wallpaper/default.png".source = ../../assets/nix-wallpaper-gear.png;
   };
 
-  home.pointerCursor = cursorTheme // {
+  home.pointerCursor = gtkCursorTheme // {
     enable = true;
     gtk.enable = true;
   };
 
   gtk = {
     enable = true;
-    inherit cursorTheme;
-    theme = {
-      package = pkgs.graphite-gtk-theme;
-      name = "Graphite-Dark";
-    };
-    iconTheme = {
-      package = pkgs.papirus-icon-theme;
-      name = "Papirus-Dark";
-    };
-    font = {
-      name = "UDEV Gothic 35NF";
-    };
+    font.name = "UDEV Gothic 35NF";
     colorScheme = "dark";
+    theme = gtkTheme;
+    iconTheme = gtkIconTheme;
+    cursorTheme = gtkCursorTheme;
   };
 }
